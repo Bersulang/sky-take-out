e204947887a1caf4e2ca0ea32292f0277c0e5fba
LazyPig <bersulang0627@gmail.com>
2025-10-15 11:12:40 +0800

    add shoppingCart apart

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -1,15 +1,15 @@
 package com.sky.controller.user;
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
 import com.sky.result.Result;
 import com.sky.service.ShoppingCartService;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user/shoppingCart")
@@ -25,6 +25,20 @@ public class ShoppingCartController {
     public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO) {
         log.info("添加购物车：{}", shoppingCartDTO);
         shoppingCartService.add(shoppingCartDTO);
+        return Result.success();
+    }
+
+    @GetMapping("/list")
+    public Result<List<ShoppingCart>> list() {
+        log.info("查看购物车");
+        List<ShoppingCart> list = shoppingCartService.list();
+        return Result.success(list);
+    }
+
+    @DeleteMapping("/clean")
+    public Result clean() {
+        log.info("清空购物车");
+        shoppingCartService.clean();
         return Result.success();
     }
 }
--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -9,9 +9,11 @@ import com.sky.mapper.DishMapper;
 import com.sky.mapper.SetmealMapper;
 import com.sky.mapper.ShoppingCartMapper;
 import com.sky.service.ShoppingCartService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
@@ -30,39 +32,54 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
      * @param shoppingCartDTO
      */
     public void add(ShoppingCartDTO shoppingCartDTO) {
-        //先去购物车表查询当前菜品是否在购物车中，是则直接增加数量和金额，否则添加一条新数据
-        ShoppingCart shop = shoppingCartMapper.getById(shoppingCartDTO);
-
-        if (shop != null)  {
-            //当前菜品或套餐已经在购物车中，则数量加一，金额增加对应的菜品或套餐金额
-            shoppingCartMapper.updateCountAndAmount(shop);
-            return;
-        }
-
+        //判断当前菜品或套餐是否在购物车中，是则数量加一，否则添加到购物车，数量为一
         ShoppingCart shoppingCart = ShoppingCart.builder()
-                .createTime(LocalDateTime.now())
                 .userId(BaseContext.getCurrentId())
                 .build();
-        //根据id判断添加的是菜品还是套餐
-        //并根据id去数据库查询对应的菜品和套餐信息
-        if (shoppingCartDTO.getDishId() == null) {
-            Setmeal setmeal = setmealMapper.getById(shoppingCartDTO.getSetmealId());
-            shoppingCart.setAmount(setmeal.getPrice());
-            shoppingCart.setName(setmeal.getName());
-            shoppingCart.setNumber(1);
-            shoppingCart.setImage(setmeal.getImage());
-            shoppingCart.setSetmealId(shoppingCartDTO.getSetmealId());
+        BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
+
+        List<ShoppingCart> shoppingCartList = shoppingCartMapper.list(shoppingCart);
+
+        if (shoppingCartList != null && shoppingCartList.size() == 1) {
+            // 数量加一
+            shoppingCart = shoppingCartList.get(0);
+            shoppingCart.setNumber(shoppingCart.getNumber() + 1);
+            shoppingCartMapper.updateById(shoppingCart);
         } else {
-            Dish dish = dishMapper.getById(shoppingCartDTO.getDishId());
-            shoppingCart.setAmount(dish.getPrice());
-            shoppingCart.setDishId(shoppingCartDTO.getDishId());
-            shoppingCart.setName(dish.getName());
+            //根据id判断添加的是菜品还是套餐
+            //并根据id去数据库查询对应的菜品和套餐信息
+            if (shoppingCartDTO.getDishId() == null) {
+                //添加的是套餐
+                Setmeal setmeal = setmealMapper.getById(shoppingCartDTO.getSetmealId());
+                shoppingCart.setAmount(setmeal.getPrice());
+                shoppingCart.setName(setmeal.getName());;
+                shoppingCart.setImage(setmeal.getImage());
+            } else {
+                //添加的是菜品
+                Dish dish = dishMapper.getById(shoppingCartDTO.getDishId());
+                shoppingCart.setAmount(dish.getPrice());
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setImage(dish.getImage());
+            }
             shoppingCart.setNumber(1);
-            shoppingCart.setImage(dish.getImage());
-            shoppingCart.setDishFlavor(shoppingCartDTO.getDishFlavor());
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartMapper.insert(shoppingCart);
         }
+    }
 
+    @Override
+    public List<ShoppingCart> list() {
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(BaseContext.getCurrentId())
+                .build();
+        return shoppingCartMapper.list(shoppingCart);
+    }
 
-        shoppingCartMapper.add(shoppingCart);
+    @Override
+    public void clean() {
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(BaseContext.getCurrentId())
+                .build();
+        shoppingCartMapper.clean(shoppingCart);
     }
 }
